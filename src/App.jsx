f461fc6c8ebcda7231d27709e2a6b07708ecf998
Juan Cruz Caceres <cacerescallieri@gmail.com>
2024-01-09 16:07:03 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <div className="card">
+        <img src="illustration-hero.svg" alt="" className='illustration'/>
+        <article>
+          <h1>Order Summary</h1>
+          <p className='paragraph-summary'>You can now listen to million of songs, audiobooks and podcasts on any device anywhere you like!</p>
+          <div className="plan-container">
+            <div className="price-container">
+              <img src="icon-music.svg" alt="" className='icon-music'/>
+              <div>
+                <p className='plan'>Annual Plan</p>
+                <p className="price">$59.99/year</p>
+              </div>
+            </div>
+            <a className="change-button">Change</a>
+          </div>
+          <button className="proceed-button">Proceed to Payment</button>
+          <button className="cancel-button">Cancel Order</button>
+        </article>
+      </div>
+    </main>
+  )
+}
+
+export default App
